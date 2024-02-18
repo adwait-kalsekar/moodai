753fe6cbb341e8c0bc121740aa6dd34fbc7a2000
adwait-kalsekar <kalsekaradwait@gmail.com>
2024-02-18 07:46:31 -0500

    production changes

--- moodai/settings.py
+++ moodai/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = "moodai.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "moodai",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
