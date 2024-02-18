ff42c66cc7d80e313914b8f7e96a7432508e36b2
adwait-kalsekar <kalsekaradwait@gmail.com>
2024-02-18 10:07:24 -0500

    final fixes

--- posts/image_generator.py
+++ posts/image_generator.py
@@ -8,7 +8,7 @@ def generate_from_prompt(prompt1, prompt2):
     
     model = 'txt2img'
     input_data = {
-        'prompt': 'Generate an abstract image for my mood, Currently my mood is ' + prompt1 + ', I feel like' + prompt2 + 'Show an abstact image related to' + prompt2 + 'Please give very high importance to' +prompt2 + 'please set your guidance_scale parameter for' + prompt2 + 'Give high weight to' + prompt2,
+        'prompt': 'Generate an abstract image for my mood, Currently my mood is ' + prompt1 + ', and I feel like ' + prompt2 + '. Show an abstract image related to ' + prompt1 + ' and ' + prompt2,
         'negprompt': 'give images with hex code= #000000 or hex code= #FFFFFF',
         'samples': 2,
         'steps': 100,
